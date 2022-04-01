63309e9c4b200bf7f2aacab8fcfae54d9b24f201
Raphael Matias <42880393+matRaph@users.noreply.github.com>
2022-03-31 22:20:37 -0300

    Commenting Python code

--- load_img.py
+++ load_img.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from PIL import Image
 
+# Load the data variable with the selected image
 data = np.array(Image.open(r"C:\Users\rapha\OneDrive\Área de Trabalho\Facul\Arquitetura\Pitfall_Project/pitfallO.png"))
+
 linhas = data.shape[0]
 colunas = data.shape[1]
 indx=0
 x=0
 
+# Print the hexadecimal rgb code of each pixel in the image and add "lnx: .word " in each line for mips to recognize
 for i in data:
-    linha="l3n"+str(x).format('999')+": .word "
+    linha="ln0"+str(x).format('999')+": .word "
     x+=1
     for j in i:
         indx+=1
